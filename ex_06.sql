32601adf9ab12eb31660b7e2af9cd2a56d87e8c6
makhambet.torezhan <makhambet.torezhan@nu.edu.kz>
2022-05-24 15:52:38 +0600

    fix queries 6,12

--- ex_06.sql
+++ ex_06.sql
@@ -1,5 +1,6 @@
 SELECT i.invoiceid, e.firstname || ' ' || e.lastname AS Employee
 FROM Invoice i
 JOIN Customer c ON  i.CustomerId = c.CustomerId
-JOIN Employee e ON c.CustomerId = e.EmployeeId
-WHERE e.Title = 'Sales Support Agent';
+JOIN Employee e ON c.SupportRepId = e.EmployeeId
+WHERE e.Title = 'Sales Support Agent'
+ORDER BY i.invoiceid;
