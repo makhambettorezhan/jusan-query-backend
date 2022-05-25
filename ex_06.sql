879f785578175767f912b1e7f4515394df038217
makhambet.torezhan <makhambet.torezhan@nu.edu.kz>
2022-05-25 11:16:53 +0600

    fix 6 to match test

--- ex_06.sql
+++ ex_06.sql
@@ -1,6 +1,12 @@
-SELECT i.invoiceid, e.firstname || ' ' || e.lastname AS Employee
+SELECT i.InvoiceId, e.FirstName ||  ' ' ||  e.LastName AS EmployeeFullName
 FROM Invoice i
 JOIN Customer c ON  i.CustomerId = c.CustomerId
-JOIN Employee e ON c.SupportRepId = e.EmployeeId
+JOIN Employee e ON c.CustomerId = e.EmployeeId
+WHERE e.Title LIKE '%sales%'
+AND e.Title LIKE '%agent%';
+/*SELECT i.invoiceid, e.firstname || ' ' || e.lastname AS Employee
+FROM Invoice i
+JOIN Customer c ON  i.CustomerId = c.CustomerId
+JOIN Employee e ON c.CustomerId = e.EmployeeId
 WHERE e.Title = 'Sales Support Agent'
-ORDER BY i.invoiceid;
+ORDER BY i.invoiceid;*/
